b92349c1a318699da22896084863b7cbdd088a8d
Changyu_Liu <liuchangyu1111@gmail.com>
2019-11-07 09:38:24 +0800

    Create string.cc

---
+++ Chapter03-Elementary-Data-Structures/string.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include<assert.h>
+using namespace std;
+
+unsigned __strlen(const char *arr) {
+  const char *temp = arr;
+  while (*temp++)
+    ;
+  return temp - arr - 1;
+}
+
+char* __strcpy(char* des, const char* src)
+{
+	while((*des++ = *src++) != '\0')  
+		;  
+	return des;
+}
+
+
+int main(int argc, char const *argv[]) {
+  const char *arr = "123456";
+  char *temp = __strcpy(temp, arr);
+  cout << __strlen(arr) << endl;
+  printf("%s\n", temp);
+  // cout << temp << endl;
+  return 0;
+}
